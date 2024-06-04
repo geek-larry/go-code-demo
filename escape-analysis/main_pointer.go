b39d1d43de9ddfb00024aeb358c77426c9f26fbc
songyx <songyx0303@126.com>
2024-06-04 22:47:04 +0800

    [ADD] 逃逸分析

---
+++ escape-analysis/main_pointer.go
@@ -0,0 +1,24 @@
+// main_pointer.go
+package main
+
+import "fmt"
+
+type Demo struct {
+	name string
+}
+
+func createDemo(name string) *Demo {
+	d := new(Demo) // 局部变量 d 逃逸到堆
+	d.name = name
+	return d
+}
+
+func test(demo *Demo) {
+	fmt.Println(demo.name)
+}
+
+func main() {
+	demo := createDemo("demo")
+	test(demo)
+	fmt.Println(demo)
+}
