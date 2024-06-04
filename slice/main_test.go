1295d3456afb2065bab240964cfd759985fcbff1
songyx <songyx0303@126.com>
2024-06-04 21:45:11 +0800

    [ADD] slice

---
+++ slice/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import ("testing"
+// "runtime"
+)
+
+func testLastChars(t *testing.T, f func([]int) []int) {
+	t.Helper()
+	ans := make([][]int, 0)
+	for k := 0; k < 100; k++ {
+		origin := generateWithCap(128 * 1024) // 1M
+		ans = append(ans, f(origin))
+		// runtime.GC()
+	}
+	printMem(t)
+	_ = ans
+}
+
+func TestLastCharsBySlice(t *testing.T) { testLastChars(t, lastNumsBySlice) }
+func TestLastCharsByCopy(t *testing.T)  { testLastChars(t, lastNumsByCopy) }
